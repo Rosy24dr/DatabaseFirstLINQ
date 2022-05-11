31436cedead6db0e61457dae899500e61edf6618
Rosy <rosy24dr@outlook.com>
2022-05-11 15:40:16 -0400

    completed problem sixteen

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -29,8 +29,8 @@ namespace DatabaseFirstLINQ
               //ProblemTwelve();
               //ProblemThirteen();
               //ProblemFourteen();
-                ProblemFifteen();
-//            //ProblemSixteen();
+              //ProblemFifteen();
+                ProblemSixteen();
 //            //ProblemSeventeen();
 //            //ProblemEighteen();
 //            //ProblemNineteen();
@@ -230,11 +230,15 @@ namespace DatabaseFirstLINQ
             _context.SaveChanges();
         }
 
-        //        private void ProblemSixteen()
-        //        {
-        //            // Update the price of the product you created to something different using LINQ.
+        private void ProblemSixteen()
+        {
+            // Update the price of the product you created to something different using LINQ.
+            var product = _context.Products.Where(p => p.Price == 5).SingleOrDefault();
+            product.Price = 6;
+            _context.Products.Update(product);
+            _context.SaveChanges();
 
-        //        }
+        }
 
         //        private void ProblemSeventeen()
         //        {
